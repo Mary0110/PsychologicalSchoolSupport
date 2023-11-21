2a2e0102ad81333ade89b5a5a888990629df2522
mariaraniuk <mashenka0110@gmail.com>
2023-11-21 11:30:01 +0300

    refactor convention

---
+++ Services/Messaging/PsychologicalSupportPlatform.Messaging.API/Controllers/ChatsController.cs
@@ -0,0 +1,18 @@
+// using Microsoft.AspNetCore.Mvc;
+// using PsychologicalSupportPlatform.Messaging.Application.Services;
+//
+// namespace PsychologicalSupportPlatform.Messaging.API.Controllers
+// {
+//     [Route("api/[controller]")]
+//     [ApiController]
+//     public class ChatsController : ControllerBase
+//     {
+//         private readonly IChatService _chatService;
+//
+//         public ChatsController(IChatService chatService)
+//         {
+//             _chatService = chatService;
+//         }
+//         
+//     }
+// }
