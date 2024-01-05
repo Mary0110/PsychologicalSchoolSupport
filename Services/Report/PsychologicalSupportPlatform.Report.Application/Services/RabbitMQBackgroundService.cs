341aa1fcad817c9d39afcbcd033d2bdb0a694383
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2024-01-05 05:40:14 +0300

    docker-compose works

--- Services/Report/PsychologicalSupportPlatform.Report.Application/Services/RabbitMQBackgroundService.cs
+++ Services/Report/PsychologicalSupportPlatform.Report.Application/Services/RabbitMQBackgroundService.cs
@@ -11,48 +11,49 @@ using RabbitMQ.Client.Events;
 
 namespace PsychologicalSupportPlatform.Report.Application.Services;
 
- public class RabbitMQBackgroundConsumerService : BackgroundService
- {
-     private readonly IServiceProvider _services;
-     private readonly IModel _channel;
+public class RabbitMQBackgroundConsumerService : BackgroundService
+{
+    private readonly IServiceProvider _services;
+    private readonly IModel _channel;
 
     public RabbitMQBackgroundConsumerService(IServiceProvider services, IOptions<RabbitMQConfig> rabbitMQConfig)
     {
         _services = services;
-        var factory = new ConnectionFactory(){ HostName = rabbitMQConfig.Value.HostName }; 
+        var factory = new ConnectionFactory() { HostName = rabbitMQConfig.Value.HostName };
         var connection = factory.CreateConnection();
         _channel = connection.CreateModel();
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        stoppingToken.ThrowIfCancellationRequested();
-        await Task.Run(() =>
+        using (var scope = _services.CreateScope())
         {
-            _channel.QueueDeclare(queue: "meetup-info",
-                durable: false,
-                exclusive: false,
-                autoDelete: false,
-                arguments: null);
-
-            var consumer = new EventingBasicConsumer(_channel);
-
-            consumer.Received += async (sender, e) =>
+            stoppingToken.ThrowIfCancellationRequested();
+            await Task.Run(() =>
             {
-                using (var scope = _services.CreateScope())
+                _channel.QueueDeclare(queue: "meetup-info",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(_channel);
+
+                consumer.Received += async (sender, e) =>
                 {
+
                     var body = e.Body.ToArray();
                     var messageJson = Encoding.UTF8.GetString(body);
                     var message = JsonConvert.DeserializeObject<MeetupMessageObject>(messageJson);
-                    
+
                     var transientService = scope.ServiceProvider.GetRequiredService<IReportService>();
                     var result = await transientService.AddMeetupReportAsync(message);
-                }
-            };
+                };
 
-            _channel.BasicConsume(queue: "meetup-info",
-                autoAck: true,
-                consumer: consumer);
-        });
+                _channel.BasicConsume(queue: "meetup-info",
+                    autoAck: true,
+                    consumer: consumer);
+            });
+        }
     }
 }
